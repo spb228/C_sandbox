395c202fa07fe10492af4a93286d3e556252513a
spb228 <spb228@cornell.edu>
2025-10-17 22:23:48 -0400

    mutex example

--- src/threads/threads.c
+++ src/threads/threads.c
@@ -6,16 +6,27 @@
 #include "stdlib.h"
 #include "stdio.h"
 
+pthread_mutex_t mtx;
+
 void* thread_body(void* arg)
 {
-    printf("hello from first thread\n");
-    printf("passed in %d\n", *(int*)(arg));
+    printf("first thread called\n");
+    int* shared_var_ptr = (int*) arg;
+    pthread_mutex_lock(&mtx); 
+    (*shared_var_ptr)++;
+    printf("[THREAD_1] value of shared_variable = %d\n", (int)*shared_var_ptr);
+    pthread_mutex_unlock(&mtx);
     return NULL;
 }
 
 void* thread_body_2(void* arg)
 {
-    printf("hello from second thread\n");
+    printf("second thread called\n");
+    int* shared_var_ptr = (int*) arg;
+    pthread_mutex_lock(&mtx); 
+    (*shared_var_ptr) += 2;
+    printf("[THREAD_2] value of shared_variable = %d\n", (int)*shared_var_ptr);
+    pthread_mutex_unlock(&mtx);
     return NULL; 
 }
 
@@ -23,22 +34,35 @@ int main(void)
 {
     printf("starting main...\n");
 
-    pthread_t thread_1; 
-    int a = 10;
+    pthread_mutex_init(&mtx, NULL);
 
-    int result = pthread_create(&thread_1, NULL, thread_body, &a);
+    pthread_t thread_1; 
+    pthread_t thread_2; 
+
+    int shared_variable = 0;
+
+    int result = pthread_create(&thread_1, NULL, thread_body, &shared_variable);
     if (result)
     {
         printf("Error: thread could not be created...\n");
         exit(1);
     }
 
-    pthread_t thread_2; 
-    result = pthread_create(&thread_2, NULL, thread_body_2, NULL);
+    result = pthread_create(&thread_2, NULL, thread_body_2, &shared_variable);
+    if (result)
+    {
+        printf("Error: thread could not be created...\n");
+        exit(1);
+    }
 
     result = pthread_join(thread_1, NULL);
-    result = pthread_join(thread_2, NULL);
+    if (result)
+    {
+        printf("thread could not be joined...\n");
+        exit(2);
+    }
 
+    result = pthread_join(thread_2, NULL);
     if (result)
     {
         printf("thread could not be joined...\n");
@@ -46,6 +70,8 @@ int main(void)
     }
 
     printf("All threads joined\n");
+
+    pthread_mutex_destroy(&mtx);
     
     return 0;
 }
