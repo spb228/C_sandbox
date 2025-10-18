34f1ab60e60c24080a01caae787ed56fa8d56ef8
spb228 <spb228@cornell.edu>
2025-10-17 21:09:56 -0400

    adding simple thread example

--- src/threads/threads.c
+++ src/threads/threads.c
@@ -9,7 +9,14 @@
 void* thread_body(void* arg)
 {
     printf("hello from first thread\n");
+    printf("passed in %d\n", *(int*)(arg));
     return NULL;
+}
+
+void* thread_body_2(void* arg)
+{
+    printf("hello from second thread\n");
+    return NULL; 
 }
 
 int main(void)
@@ -17,21 +24,28 @@ int main(void)
     printf("starting main...\n");
 
     pthread_t thread_1; 
+    int a = 10;
 
-    int result = pthread_create(&thread_1, NULL, thread_body, NULL);
+    int result = pthread_create(&thread_1, NULL, thread_body, &a);
     if (result)
     {
         printf("Error: thread could not be created...\n");
         exit(1);
     }
 
+    pthread_t thread_2; 
+    result = pthread_create(&thread_2, NULL, thread_body_2, NULL);
+
     result = pthread_join(thread_1, NULL);
+    result = pthread_join(thread_2, NULL);
 
     if (result)
     {
         printf("thread could not be joined...\n");
         exit(2);
     }
+
+    printf("All threads joined\n");
     
     return 0;
 }
