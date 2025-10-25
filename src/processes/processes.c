bd1ef5c6201f2d5a5170543a1c66d4e350347601
spb228 <spb228@cornell.edu>
2025-10-24 20:10:22 -0400

    adding minor comments

--- src/processes/processes.c
+++ src/processes/processes.c
@@ -6,7 +6,7 @@
 int main(int argc, char** argv)
 {
     char* args[] = {"echo", "Hello", "World!", 0}; 
-    execvp("echo", args); 
+    execvp("echo", args); // create new process that replaces this process
     printf("execvp failed..."); // if this is called, execvp call failed
     return 0;
 }
